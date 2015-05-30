bb7b876bbe9bf2086c2c903048c295d0b4647812
Andre Ganske <andre.g.ganske@gmail.com>
2015-05-30 13:51:27 -0300

    nova estrutura usando angular e bootstrap

--- bootstrap/br-content/js/controllers/login.js
+++ bootstrap/br-content/js/controllers/login.js
@@ -4,23 +4,11 @@ angular.module('blackroom')
 
 .controller('loginController', function($scope, $rootScope, $routeParams, $location, $http, Data) {
 
-	$scope.login = {};
-	$scope.signup = {};
+    $scope.login = {};
+    $scope.signup = {};
 
-	$scope.doLogin = function (customer) {
-		Data.post('login', {customer: customer}).then(function(results) {
-			Data.toast(results);
-			if (results.status == "success") {
-				$location.path('dashboard');
-			}
-		});
-	};
-
-	$scope.signup = {email:'', password:'', name:''};
-    $scope.signUp = function (customer) {
-        Data.post('signUp', {
-            customer: customer
-        }).then(function (results) {
+    $scope.doLogin = function(customer) {
+        Data.post('login', {customer: customer}).then(function(results) {
             Data.toast(results);
             if (results.status == "success") {
                 $location.path('dashboard');
@@ -28,8 +16,18 @@ angular.module('blackroom')
         });
     };
 
-    $scope.logout = function () {
-        Data.get('logout').then(function (results) {
+    $scope.signup = {email:'', password:'', name:''};
+    $scope.signUp = function(customer) {
+        Data.post('signUp', {customer: customer}).then(function(results) {
+            Data.toast(results);
+            if (results.status == "success") {
+                $location.path('dashboard');
+            }
+        });
+    };
+
+    $scope.logout = function() {
+        Data.get('logout').then(function(results) {
             Data.toast(results);
             $location.path('login');
         });
--- bootstrap/br-content/js/data/data.js
+++ bootstrap/br-content/js/data/data.js
@@ -2,39 +2,39 @@
 
 angular.module('blackroom.data', [])
 
-.factory("Data", ['$http', 'toaster', function($http, toaster) {
-	
-	var serviceBase = '/blackroom/br-content/api',
-		obj = {};
+.factory("Data", ['$http', 'toaster',
+    function($http, toaster) {
 
-	obj.toast = function(data) {
-		toaster.pop(data.status, "", data.message, 10000, 'trustedHtml');
-	}
+    	var serviceBase = 'api/v1/';
+    	var obj = {};
 
-	obj.get = function(query) {
-		return $http.get(serviceBase + query).then(function(results) {
-			return results.data;
-		});
-	}
+    	obj.toast = function(data) {
+    		toaster.pop(data.status, "", data.message, 10000, 'trustedHtml');
+    	}
 
-	obj.post = function(query, object) {
-		return $http.post(serviceBase + query, object).then(function(results) {
-			return results.data;
-		});
-	}
+    	obj.get = function(q) {
+    		return $http.get(serviceBase + q).then(function(results) {
+    			return results.data;
+    		});
+    	};
 
-	obj.put = function(query, object) {
-		return $http.put(serviceBase + query, object).then(function(results) {
-			return results.data;
-		});
-	}
+    	obj.post = function(q, object) {
+    		return $http.post(serviceBase + q, object).then(function(results) {
+    			return results.data;
+    		});
+    	};
 
-	obj.delete = function(query) {
-		return $http.delete(serviceBase + query).then(function(results) {
-			return results.data;
-		});
-	}
+    	obj.put = function(q, object) {
+    		return $http.put(serviceBase + q, object).then(function(results) {
+    			return results.data;
+    		});
+    	};
 
-	return obj;
+    	obj.delete = function(q) {
+    		return $http.delete(serviceBase + q).then(function(results) {
+    			return results.data;
+    		});
+    	};
 
+    	return obj;
 }]);
