ebcfac5864de9a3fe06bfda2f633bc16c66c67cd
Andre Ganske <andre.g.ganske@gmail.com>
2015-05-31 14:25:25 -0300

    colors and menu style

--- bootstrap/br-landing/js/blackroom.js
+++ bootstrap/br-landing/js/blackroom.js
@@ -3,8 +3,10 @@
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $('.navbar-brand').show();
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $('.navbar-brand').hide();
     }
 });
 
